e33dc3745a112f0213f6154fa04114209377230c
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-19 23:22:27 +0200

    commit 18 : Adding more sounds for information for room 2 and room 3

---
+++ Assets/scripts/audioblock.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioblock : MonoBehaviour
+{
+    public GameObject Instructions;
+    public GameObject ThisTrigger;
+    public AudioSource Information1;
+    public AudioSource Information2;
+    public AudioSource Information3;
+    public bool Action = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instructions.SetActive(false);
+        //ThisTrigger.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instructions.SetActive(true);
+            Action = true;
+        }
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instructions.SetActive(false);
+            ThisTrigger.SetActive(false);
+            Action = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+               // ThisTrigger.SetActive(true);
+                Instructions.SetActive(true);
+                Information1.Play();
+                Action = false;
+
+
+            }
+            else if (Action == false)
+            {
+
+               // ThisTrigger.SetActive(false);
+                Instructions.SetActive(false);
+
+
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (Action == true)
+            {
+               // ThisTrigger.SetActive(true);
+                Instructions.SetActive(true);
+                Information2.Play();
+                Action = false;
+
+
+            }
+            else if (Action == false)
+            {
+
+               // ThisTrigger.SetActive(false);
+                Instructions.SetActive(false);
+
+
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (Action == true)
+            {
+               // ThisTrigger.SetActive(true);
+                Instructions.SetActive(true);
+                Information3.Play();
+                Action = false;
+
+
+            }
+            else if (Action == false)
+            {
+
+               // ThisTrigger.SetActive(false);
+                Instructions.SetActive(false);
+
+
+            }
+
+        }
+    }
+}
